948be7c004391c77119debebfacf73b60ba93eb7
Cole Doster <wcdoster@gmail.com>
2020-02-24 20:30:12 -0600

    Test update

--- js/App.js
+++ js/App.js
@@ -4,7 +4,7 @@ export default class App extends React.Component {
     render(){
         return(
             <div>
-                Hello World! How are you??? Nice to see you
+                Hello World
             </div>
         )
     }
